11a0679d6ceb0d6e5d670d25b91de07957bbec67
eleeisb <elee@systemsbiology.org>
2022-09-12 01:41:56 -0700

    tier based variable configured

--- shell/gcloud-pull-tier-files.sh
+++ shell/gcloud-pull-tier-files.sh
@@ -1,4 +1,5 @@
 gsutil cp "gs://${DEPLOYMENT_BUCKET}/${WEBAPP_RUNTIME_SA_KEY}" ./privatekey.json
+gsutil cp "gs://${DEPLOYMENT_BUCKET}/client_secret.json" ./client_secret.json
 gsutil cp "gs://${DEPLOYMENT_BUCKET}/sitemap.xml" ./sitemap.xml
 gsutil cp "gs://${DEPLOYMENT_BUCKET}/urllist.txt" ./urllist.txt
 gsutil cp "gs://${DEPLOYMENT_BUCKET}/app.yaml" ./app.yaml
