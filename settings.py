178a6a0a1f6db68d8f918f98a5db3128389a2eac
eleeisb <elee@systemsbiology.org>
2022-09-13 12:20:17 -0700

    logging config

--- settings.py
+++ settings.py
@@ -18,7 +18,21 @@ import os
 from flask import json
 import utils
 
-
+LOG_CONFIG = {
+    'version': 1,
+    'formatters': {'default': {
+        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+    }},
+    'handlers': {'wsgi': {
+        'class': 'logging.StreamHandler',
+        'stream': 'ext://flask.logging.wsgi_errors_stream',
+        'formatter': 'default'
+    }},
+    'root': {
+        'level': 'INFO',
+        'handlers': ['wsgi']
+    }
+}
 M_C_DESC_FILE = 'M_C_DESC.TXT.LIST'
 M_P_DESC_FILE = 'M_P_DESC.TXT.LIST'
 M_G_DESC_HG19_FILE = 'M_G_DESC_HG19.TXT.LIST'
